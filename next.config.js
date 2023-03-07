fc388b6b655caa8657f562e7ec1fa512c1285bc5
inamfalahuddin <inamfalahuddin06@gmail.com>
2023-03-08 01:33:58 +0700

    menambahkan hostname pada next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "test.solotechnopark.id",
+        port: "3000",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
